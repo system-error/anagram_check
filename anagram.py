e72271fd933eedf0dab9987bb37921ab537bf4ee
Christos Dousmanopoulos <system-error@users.noreply.github.com>
2018-06-28 11:27:41 +0300

    Updated
    now works with words that have empty spaces

--- anagram.py
+++ anagram.py
@@ -5,19 +5,34 @@ def checkForAnagram(word1,word2):
     str1 =  word1.lower()
     str2 =  word2.lower()
 
+    list1 = list(word1)
+    list2 = list(word2)
     str1 = sorted(str1)
     str2 = sorted(str2)
+    # print(type(str1))
+    # print()
+    # print(str2)
+
+    for value1,value2 in zip(str1,list1):
+        if value1 ==' ' or value1==',':
+            str1.remove(value1)
+        elif value2 ==' ' or value2==',':    
+            list1.remove(value2)
+            
+    for value1,value2 in zip(str2,list2):
+        if value1 ==' ' or value1 ==',':
+            str2.remove(value1)
+        elif value2 ==' ' or value2==',':       
+            list2.remove(value2)
 
     if str1 == str2:
         print('True')
+        list1 = ''.join(list1)
+        list2 = ''.join(list2)
+        print(list1)          
+        print(list2)
     else:    
         print('false') 
+   
 
-    # for letter1,letter2 in zip(str1,str2):
-    #     if letter1 == letter2:
-    #         print('True')
-    #     else:    
-    #         print('false')    
-        
-
-checkForAnagram(word1,word2)
+checkForAnagram(word1,word2) 
